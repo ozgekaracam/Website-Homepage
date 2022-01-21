dea6adba09ab31042f83192ced9b4b3d7359c04c
ozgekaracam <90089950+ozgekaracam@users.noreply.github.com>
2022-01-21 11:31:56 +0300

    Update file.js

--- file.js
+++ file.js
@@ -1,3 +1,42 @@
+const tableBody = document.querySelector("#phonesTableBody");
+document.addEventListener("DOMContentLoaded", () => { loadPhoneTable(); });
+document.addEventListener("addNewRow", () => { loadPhoneTable(); });
+
+function loadPhoneTable(){
+  const request = new XMLHttpRequest();
+  request.open("GET","https://wt.ops.labs.vu.nl/api22/ccc90d56");
+  request.onload = () => {
+    try{
+      const json = JSON.parse(request.responseText);
+      populatePhoneTable(json);
+    }
+     catch(e){
+       console.warn("Couldn't load phones! :(");
+     }
+  };
+  request.send();
+}
+function populatePhoneTable(json){
+  console.log(json);
+  json.forEach(obj => {
+    const tr = document.createElement("tr");
+    Object.entries(obj).forEach(([key, value]) => {
+      if(`${key}` != "id")
+      {
+        const td = document.createElement("td");
+        console.log(td);
+        td.textContent = `${value}`;
+        tr.appendChild(td);
+        console.log(tr);
+        console.log(`${key} ${value}`);
+      }
+    });
+  });
+  console.log('-------------------');
+  tableBody.appendChild(tr);
+  console.log('----------appendError---------');
+  console.log(phonesTableBody);
+}
 //sorting function source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
